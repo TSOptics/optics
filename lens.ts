25200c6579fd78d14f5f0da17d9032d19bcd244f
Vincent Quillien <vincent.quillien@dougs.fr>
2020-09-16 18:48:51 +0200

    save

--- lens.ts
+++ lens.ts
@@ -1,3 +1,5 @@
+import {DeepNonNullable} from 'ts-essentials'
+
 type Path<T, R> = string[]
 
 function get<T extends { [key: string]: any }, R>(path: Path<T, R>, root: T): R {
@@ -19,8 +21,8 @@ function set<T extends { [key: string]: any }, R>(path: Path<T, R>, root: T, val
 
 function root<T>(): Path<T, T> { return [] }
 
-function path<TRoot, A, B extends keyof A>(root: Path<TRoot, A>, prop: B): Path<TRoot, A[B]>;
-function path<TRoot, A, B extends keyof A, C extends keyof A[B]>(root: Path<TRoot, A>, b: B, c: C): Path<TRoot, A[B][C]>;
+function path<TRoot, A, B extends keyof NonNullable<A>>(root: Path<TRoot, A>, prop: B): Path<TRoot, NonNullable<A>[B]>;
+function path<TRoot, A, B extends keyof NonNullable<A>, C extends keyof NonNullable<NonNullable<A>[B]>>(root: Path<TRoot, A>, b: B, c: C): Path<TRoot, NonNullable<NonNullable<A>[B]>[C]>;
 function path<TRoot, A, B extends keyof A, C extends keyof A[B], D extends keyof A[B][C]>(root: Path<TRoot, A>, b: B, c: C, d: D): Path<TRoot, A[B][C][D]>;
 function path<TRoot, A, B extends keyof A, C extends keyof A[B], D extends keyof A[B][C], E extends keyof A[B][C][D]>(root: Path<TRoot, A>, b: B, c: C, d: D, e: E): Path<TRoot, A[B][C][D][E]>;
 
@@ -32,14 +34,16 @@ function compose<T, U, R>(path1: Path<T, U>, path2: Path<U, R>): Path<T, R> {
     return [...path1, ...path2];
 }
 
-type Test = { a: { b: { c: number; e: string, f: {g: {h: {i: number}}} } } }
+type Test = { a?: { b?: { c?: number; e: string, f: {g: {h: {i: number}}} } }, j?: boolean }
 
 const example: Test = { a: { b: { c: 42, e: 'yolo', f: {g: {h: {i: 1798}}} } } }
 
 
-const pathToC = path(root<Test>(), 'a', 'b', 'c')
-const pathToB = path(root<Test>(), 'a', 'b')
-console.log(get(pathToC, example))
-console.log(set(pathToC, example, 84))
-const pathToI = path(pathToB, 'f', 'g', 'h', 'i')
-console.log(set(pathToI, example, 1993))
+const pathToA = path(root<Test>(), 'a')
+const pathToB = path(pathToA, 'b')
+
+type TailType<T, U> = undefined | null extends T ? undefined | null | U : undefined extends T ? undefined | U : null extends T ? null | U : T;
+
+type Res = TailType<string | null | undefined, object>
+  
+type T = NonNullable<Test['j']>

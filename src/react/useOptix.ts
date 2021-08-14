154a58a8abafedb1f6e9fdf2d7792d3651c3cd93
Vincent Quillien <vincent.quillien@dougs.fr>
2021-08-14 19:50:01 +0200

    Add optional equalityFn to useOptix to skip rerender if true

--- src/react/useOptix.ts
+++ src/react/useOptix.ts
@@ -1,25 +1,38 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
-import { Optix, partial } from '../lens';
+import { Optix, partial, total } from '../lens';
 import { StoreContext } from './createStore';
 
-const useOptix = <T, TLensType extends partial>(optix: Optix<T, TLensType>) => {
+type Focus<T, TLensType extends partial> = TLensType extends total ? T : T | undefined;
+type EqualityFn<T, TLensType extends partial> = (
+    oldValue: Focus<T, TLensType>,
+    newValue: Focus<T, TLensType>,
+) => boolean;
+
+const useOptix = <T, TLensType extends partial>(optix: Optix<T, TLensType>, equalityFn?: EqualityFn<T, TLensType>) => {
     const { root, setRoot, subscriptions } = useContext(StoreContext);
 
     const [slice, setSlice] = useState(optix.get(root.ref));
 
     useEffect(() => {
-        const subscription = (newRoot: any) => setSlice(optix.get(newRoot));
+        const subscription = (newRoot: any) =>
+            setSlice((oldSlice) => {
+                const newSlice = optix.get(newRoot);
+                return !equalityFn || !equalityFn(oldSlice, newSlice) ? newSlice : oldSlice;
+            });
         subscriptions.add(subscription);
         return () => {
             subscriptions.delete(subscription);
         };
-    }, []);
+    }, [optix, subscriptions, equalityFn]);
 
-    const setter = useCallback((value: T | ((prevState: typeof slice) => T)) => {
-        const newValue =
-            typeof value !== 'function' ? value : (value as (prevState: typeof slice) => T)(optix.get(root.ref));
-        setRoot(optix.set(newValue, root.ref));
-    }, []);
+    const setter = useCallback(
+        (value: T | ((prevState: typeof slice) => T)) => {
+            const newValue =
+                typeof value !== 'function' ? value : (value as (prevState: typeof slice) => T)(optix.get(root.ref));
+            setRoot(optix.set(newValue, root.ref));
+        },
+        [optix, root, setRoot],
+    );
 
     return [slice, setter] as [typeof slice, typeof setter];
 };

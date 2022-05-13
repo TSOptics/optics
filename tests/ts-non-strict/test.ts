3da17567641e5deb1bcc49a715d10540455e13fb
Vincent Quillien <vincent.quillien@dougs.fr>
2022-05-13 18:37:39 +0200

    Delete useless test

--- tests/ts-non-strict/test.ts
+++
@@ -1,5 +0,0 @@
-import { Optic } from '../../src/Optic';
-import { optic } from '../../src/constructors';
-import { total } from '../../src/types';
-
-const onD: Optic<string, total> = optic<{ a: { b: { c: { d: string } } } }>().focus('a.b.c.d');

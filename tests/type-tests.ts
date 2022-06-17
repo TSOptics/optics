520c163ae828bb431c54dc74bc255bc36d84958c
Vincent Quillien <vincent.quillien@dougs.fr>
2022-06-17 20:24:01 +0200

    Fix issues with RecursivePath type

--- tests/type-tests.ts
+++ tests/type-tests.ts
@@ -1,6 +1,6 @@
 import { optic } from '../src/constructors';
 import { Optic } from '../src/Optic';
-import { total, partial, mapped } from '../src/types';
+import { total, partial, mapped, Path } from '../src/types';
 import { noop } from '../src/utils';
 
 const expectTotal: <A = any, S = any>(o: Optic<A, total, S>) => void = noop;
@@ -105,14 +105,12 @@ describe('lens', () => {
     });
 });
 describe('paths', () => {
-    type RecursiveRecord<Depth extends number, Levels extends number[] = []> = Levels['length'] extends Depth
-        ? 42
-        : { [Level in Levels['length'] as `level_${Level}`]: RecursiveRecord<Depth, [0, ...Levels]> };
+    type NestedRecord<Depth, Levels extends any[] = []> = Levels['length'] extends Depth
+        ? string
+        : { [P in Levels['length'] as `level${P}`]: NestedRecord<Depth, [any, ...Levels]> };
 
-    it('should handle deep paths', () => {
-        type DeepRecord = RecursiveRecord<30>;
-        const onLastLevel = optic<DeepRecord>().focus(
-            'level_0.level_1.level_2.level_3.level_4.level_5.level_6.level_7.level_8.level_9.level_10.level_11.level_12.level_13.level_14.level_15.level_16.level_17.level_18.level_19.level_20.level_21.level_22.level_23.level_24.level_25.level_26.level_27.level_28.level_29',
-        );
+    it('should handle big objects without reaching the type instantiation depth limit', () => {
+        type Record30Deep = NestedRecord<30>;
+        type AllPaths = Path<Record30Deep>;
     });
 });

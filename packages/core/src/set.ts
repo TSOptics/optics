38cdc8eeb7f0a67015f8c5e4159a360f06b6cca5
Vincent Quillien <vincentquillien01@gmail.com>
2023-11-11 23:40:44 +0100

    fix: ensure set doesn't skip the following reduce after the first one

--- packages/core/src/set.ts
+++ packages/core/src/set.ts
@@ -6,56 +6,50 @@ type Continuation = (s: any) => void;
 export type ReduceValue<T = any> = { readonly value: T; [contSymbol]: Continuation };
 
 export const set = <A, S>(a: A | ((prev: A) => A), s: S, lenses: Lens[]): S => {
-    const setWithCont = <A, S>(s: S, lenses: Lens[], cont: Continuation): void => {
-        const rec = (s: any, lenses: Lens[], cont: Continuation): void => {
-            const [lens, ...tailLenses] = lenses;
-            if (!lens) {
-                return cont(typeof a === 'function' ? (a as (prev: any) => A)(s) : (a as any));
-            }
-            if (lens.type === 'fold') {
-                reduceData.lenses = lenses;
-                reduceData.values.push({ value: s, [contSymbol]: cont });
-                return;
-            }
-            const slice = lens.get(s);
-            if ((slice === undefined || slice === null) && tailLenses.length > 0) return;
-            if (lens.type === 'map') {
-                const newArray = [...slice];
-                (slice as any[]).forEach((elem, i) => {
-                    if ((elem !== undefined && elem !== null) || tailLenses.length === 0) {
-                        rec(elem, tailLenses, (newElem) => (newArray[i] = newElem));
-                    }
-                });
-                const callCont = () => cont(newArray.some((elem, i) => elem !== slice[i]) ? newArray : slice);
-                reduceData.values.length > 0 ? reduceData.mappedConts.push(callCont) : callCont();
-                return;
-            }
-            rec(slice, tailLenses, (newSlice) => cont(newSlice === slice ? s : lens.set(newSlice, s)));
-        };
-
-        const reduceData: ReduceData = {
-            values: [],
-            lenses: [],
-            mappedConts: [],
-        };
-
-        rec(s, lenses, cont);
-        if (reduceData.values.length > 0) {
-            reduce(reduceData);
+    const setWithCont = (s: any, lenses: Lens[], cont: Continuation): void => {
+        const [lens, ...tailLenses] = lenses;
+        if (!lens) {
+            return cont(typeof a === 'function' ? (a as (prev: any) => A)(s) : (a as any));
         }
+        if (lens.type === 'fold') {
+            reduceData.lenses = lenses;
+            reduceData.values.push({ value: s, [contSymbol]: cont });
+            return;
+        }
+        const slice = lens.get(s);
+        if ((slice === undefined || slice === null) && tailLenses.length > 0) return;
+        if (lens.type === 'map') {
+            const newArray = [...slice];
+            (slice as any[]).forEach((elem, i) => {
+                if ((elem !== undefined && elem !== null) || tailLenses.length === 0) {
+                    setWithCont(elem, tailLenses, (newElem) => (newArray[i] = newElem));
+                }
+            });
+            stackedMappings.push(() => cont(newArray.some((elem, i) => elem !== slice[i]) ? newArray : slice));
+            return;
+        }
+        setWithCont(slice, tailLenses, (newSlice) => cont(newSlice === slice ? s : lens.set(newSlice, s)));
     };
 
-    type ReduceData = { values: ReduceValue[]; lenses: Lens[]; mappedConts: (() => void)[] };
-    const reduce = ({ values, lenses: [reduceLens, ...tailLenses], mappedConts }: ReduceData) => {
+    const stackedMappings: (() => void)[] = [];
+    const reduceData: { values: ReduceValue[]; lenses: Lens[] } = { values: [], lenses: [] };
+
+    const reduce = () => {
+        const [reduceLens, ...tailLenses] = reduceData.lenses;
+        const { values } = reduceData;
         const reduced: ReduceValue[] | (ReduceValue | undefined) = reduceLens.get(values);
+        reduceData.values = [];
         if (!reduced) return;
         (Array.isArray(reduced) ? reduced : [reduced]).forEach(({ value, [contSymbol]: cont }) => {
             setWithCont(value, tailLenses, cont);
         });
-        mappedConts.forEach((cont) => cont());
     };
 
     let result = s;
     setWithCont(s, lenses, (_result) => (result = _result));
+    while (reduceData.values.length > 0) {
+        reduce();
+    }
+    stackedMappings.forEach((cont) => cont());
     return result;
 };

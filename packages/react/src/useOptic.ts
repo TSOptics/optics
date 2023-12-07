812eb27ad138f2070f58ce059509191f1cc36d65
Vincent Quillien <vincentquillien01@gmail.com>
2023-12-07 18:29:22 +0100

    fix(react): fix useOptic infering union of normalized and denormalized type when no options are passed

--- packages/react/src/useOptic.ts
+++ packages/react/src/useOptic.ts
@@ -1,21 +1,34 @@
 import { Dispatch, SetStateAction, useCallback, useMemo, useSyncExternalStore } from 'react';
-import { OpticScope, Optic, GetStateOptions, ResolvedType, ReadOptic } from '@optics/state';
+import {
+    OpticScope,
+    Optic,
+    GetStateOptions,
+    ResolvedType,
+    ReadOptic,
+    GetOpticFocus,
+    GetOpticScope,
+    FocusedValue,
+} from '@optics/state';
 
 export type UseOpticOptions = GetStateOptions;
 
-export function useOptic<T, TScope extends OpticScope, TOptions extends UseOpticOptions | undefined>(
-    optic: Optic<T, TScope>,
-    options?: TOptions,
-): [ResolvedType<T, TScope, TOptions>, Dispatch<SetStateAction<T>>];
-export function useOptic<T, TScope extends OpticScope, TOptions extends UseOpticOptions | undefined>(
-    optic: ReadOptic<T, TScope>,
-    options?: TOptions,
-): [ResolvedType<T, TScope, TOptions>];
-export function useOptic<T, TScope extends OpticScope, TOptions extends UseOpticOptions | undefined>(
-    optic: Optic<T, TScope> | ReadOptic<T, TScope>,
-    options?: TOptions,
-) {
-    const { denormalize }: UseOpticOptions = { denormalize: false, ...(options ?? {}) };
+export function useOptic<TOptic extends ReadOptic<any, OpticScope>>(
+    optic: TOptic,
+): [GetOpticFocus<TOptic>, GetOpticScope<TOptic>] extends [infer TFocus, infer TScope extends OpticScope]
+    ? ReadOptic<TFocus, TScope> extends TOptic
+        ? [FocusedValue<TFocus, TScope>]
+        : [FocusedValue<TFocus, TScope>, Dispatch<SetStateAction<TFocus>>]
+    : never;
+export function useOptic<TOptic extends ReadOptic<any, OpticScope>, TOptions extends UseOpticOptions>(
+    optic: TOptic,
+    options: TOptions,
+): [GetOpticFocus<TOptic>, GetOpticScope<TOptic>] extends [infer TFocus, infer TScope extends OpticScope]
+    ? ReadOptic<TFocus, TScope> extends TOptic
+        ? [ResolvedType<TFocus, TScope, TOptions>]
+        : [ResolvedType<TFocus, TScope, TOptions>, Dispatch<SetStateAction<TFocus>>]
+    : never;
+export function useOptic<TOptic extends ReadOptic<any, OpticScope>>(optic: TOptic, options?: UseOpticOptions) {
+    const { denormalize } = { denormalize: false, ...(options ?? {}) };
 
     const subscribe = useCallback(
         (listener: () => void) => optic.subscribe(listener, { denormalize }),
@@ -26,7 +39,7 @@ export function useOptic<T, TScope extends OpticScope, TOptions extends UseOptic
 
     const slice = useSyncExternalStore(subscribe, getSnapshot);
 
-    const setSlice = useMemo(() => (optic as Optic<T, TScope>)?.set.bind(optic), [optic]);
+    const setSlice = useMemo(() => (optic as Optic<any, OpticScope>)?.set.bind(optic), [optic]);
 
     return [slice, setSlice];
 }
